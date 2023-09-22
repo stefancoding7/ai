c646601346737ca43588ecb30971d214a57e7cb3
Stefan Caky <stefan.caky777@gmail.com>
2023-09-22 22:37:54 +0100

    upda

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -283,6 +283,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('my-account') }}">
+                                       Bubby Boo chat
+                                    </a>
                                      <a class="dropdown-item" href="{{ route('my-account') }}">
                                        My Account
                                     </a>
