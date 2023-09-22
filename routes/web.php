62926728804ebad4933f87e98fa781af8230e8dd
Stefan Caky <stefan.caky777@gmail.com>
2023-09-22 21:08:14 +0100

    upda

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
-//Auth::routes(['register' => false]);
+//Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
