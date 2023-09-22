3fe6ce6515228c842a133fa896ddfef9d233f33b
Stefan Caky <stefan.caky777@gmail.com>
2023-09-22 20:26:53 +0100

    upda

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card" style="height: 100vh;">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">AI Stefancoding chat</div>
 
                 <div class="card-body">
                     @livewire('chat-board')
-
-                    
                 </div>
             </div>
         </div>
--- resources/views/livewire/chat-board.blade.php
+++ resources/views/livewire/chat-board.blade.php
@@ -1,5 +1,9 @@
 <div>
     {{$in_message}}
-    <input wire:model="out_message" type="text" class="form-control">
-    <button class="btn btn-primary" wire:click="submit">Send</button>
+    <div class="input-group mb-3">
+        <input wire:model="out_message" type="text" class="form-control" placeholder="Message..." aria-label="Recipient's username" aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2" wire:click="submit">Send</button>
+    </div>
+
+    
 </div>
