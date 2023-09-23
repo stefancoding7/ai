a1fafa053f541b77bb6ef61c9e3ac223cb577b8f
Stefan Caky <stefan.caky777@gmail.com>
2023-09-23 13:14:22 +0100

    upda

--- resources/views/livewire/chat-board.blade.php
+++ resources/views/livewire/chat-board.blade.php
@@ -57,9 +57,9 @@
 
         </div>
         <div class="card-footer">
-            <div class="input-group mb-3">
+            <div class="input-group mb-3" style="height: 50px;">
             <input wire:model="out_message" type="text" class="form-control" placeholder="Message..."  aria-describedby="button-addon2">
-            <button wire:click="submit" wire:keydown.enter="submit" class="btn btn-outline-secondary" type="button" id="button-addon2">Send</button>
+            <button wire:click="submit" wire:keydown.enter="submit" class="btn btn-outline-secondary" type="button" id="button-addon2" style="color: white; width: 100px;">Send</button>
             </div>
 
         </div>
