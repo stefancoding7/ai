48cbb8ce18e6e149b6bd000c7bf1391c35b6b818
Stefan Caky <stefan.caky777@gmail.com>
2023-09-30 20:48:04 +0100

    upda

--- resources/views/livewire/chat-input.blade.php
+++ resources/views/livewire/chat-input.blade.php
@@ -32,8 +32,8 @@
     
     <form wire:submit.prevent="submit">
         <div class="input-group mb-3" style="height: 40px;">
-            
-                <input wire:model="out_message" type="text" class="form-control" placeholder="Message..."  aria-describedby="button-addon2" style="border-radius: 20px 0 3px 20px; ">
+                <textarea wire:model="out_message"></textarea>
+                {{-- <input  type="text" class="form-control" placeholder="Message..."  aria-describedby="button-addon2" style="border-radius: 20px 0 3px 20px; "> --}}
                 @if($selected_gpt == 'create-image')
                     <span class="input-group-text btn btn-outline-secondary" ><label><input type="file" wire:model="photo" style="display: none;"><i class="bi bi-image" style="font-size: 25px; color: white; margin-top: 15px; padding: 10px;"></label></i></span>
                 @endif
