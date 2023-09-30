0b6d056e223f88d9fb20f33d4901efac4982280e
Stefan Caky <stefan.caky777@gmail.com>
2023-09-30 20:56:36 +0100

    upda

--- resources/views/livewire/chat-board.blade.php
+++ resources/views/livewire/chat-board.blade.php
@@ -103,7 +103,7 @@
                             </div>
                             <div class="col-4 m-0 p-0">
                                 <div class="d-grid gap-2">
-                                    <button wire:click="setSelected_gpt('gpt-3.5-turbo')" class="btn btn-{{$selected_gpt != 'gpt-3.5-turbo' ? 'outline-' : ''}}info" style="border-radius: 20px; font-size: 12px; {{$selected_gpt != 'gpt-3.5-turbo' ? 'color: black;' : ''}}">Website</button>
+                                    <button wire:click="setSelected_gpt('gpt-3.5-turbo')" class="btn btn-{{$selected_gpt != 'gpt-3.5-turbo' ? 'outline-' : ''}}info" style="border-radius: 20px; font-size: 12px; {{$selected_gpt != 'gpt-3.5-turbo' ? 'color: black;' : ''}}">WEBSITE</button>
                                 </div>
                                 
                             </div>
